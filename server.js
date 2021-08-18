f6a324f49853d32d9820b5fa64e56c79ca0976a3
Rachel <rtc145@gmail.com>
2021-08-17 21:57:14 -0400

    fixed case sensitive gitignore

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
+/* app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+); */
 
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
